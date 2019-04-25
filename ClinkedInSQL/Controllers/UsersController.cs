22fa1f531766474115fe5b5c70071ea692b900f9
Colin White <clwhite814@gmail.com>
2019-04-24 20:54:13 -0500

    initial commit

---
+++ ClinkedInSQL/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinkedInSQL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetAllUsers()
+        {
+            var users = _userRepository.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ ClinkedInSQL/Data/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedInSQL.Models;
+
+namespace ClinkedInSQL.Data
+{
+    public class UserRepository
+    {
+        public List<User>
+    }
+}
